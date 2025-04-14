78535db59a04e502bb8a1dc8fb6631e1e2025408
Joe Latessa <latessa@users.noreply.github.com>
2025-04-13 23:54:03 -0400

    rename files

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -1,10 +1,9 @@
-class Hepmc3 < Formula
+class Hepmc326 < Formula
   desc "C++ event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/"
-  url "https://hepmc.web.cern.ch/releases/HepMC3-3.2.7.tar.gz"
-  sha256 "587faa6556cc54ccd89ad35421461b4761d7809bc17a2e72f5034daea142232b"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.6.tar.gz"
+  sha256 "248f3b5b36dd773844cbe73d51f60891458334b986b259754c59dbf4bbf1d525"
   license "LGPL-3.0-or-later"
-  revision 1
 
   livecheck do
     url "https://hepmc.web.cern.ch/hepmc/"
@@ -13,13 +12,13 @@ class Hepmc3 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/davidchall/hep"
-    sha256 cellar: :any, arm64_sonoma: "837227225c31931c24ae1f7849998206a5310f99856320c4385c53cd369513f8"
-    sha256 cellar: :any, ventura:      "c646245722ef801587157cb3cdcc1195e92a6f5f89d52ebda5f90705c1abb3de"
+    rebuild 1
+    sha256 cellar: :any, monterey: "c23ed0a007c130b6b5e3866cd299b63ab9457a5cba0c1d04383865b3c576a28e"
+    sha256 cellar: :any, big_sur:  "2248251cac9e1d4783a964dbffbfe047e4ba572ba8de86bb7267b9235f707a6f"
   end
 
   option "with-test", "Test during installation"
 
-  depends_on "cmake" => [:build, :test]
   depends_on "coreutils" # HepMC3-config uses greadlink
   depends_on "python@3.10"
   depends_on "protobuf" => :optional
@@ -52,6 +51,7 @@ class Hepmc3 < Formula
 
   test do
     assert_equal prefix.to_s, shell_output(bin/"HepMC3-config --prefix").strip
+
     system Formula["python@3.10"].opt_bin/python, "-c", "import pyHepMC3"
 
     cp_r share/"doc/HepMC3/examples/.", testpath
